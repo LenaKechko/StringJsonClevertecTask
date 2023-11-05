63e049ad7e38075cc5d229c1148ee51661de995b
Elena Kechko <ekechko@gmail.com>
2023-11-05 23:09:32 +0300

    Creating entities and README.md

---
+++ src/main/java/ru/clevertec/entity/Subject.java
@@ -0,0 +1,16 @@
+package ru.clevertec.entity;
+
+import java.util.Map;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Subject {
+    private UUID id;
+    private String nameSubject;
+    private Map<Speciality, Integer> specialityWithNumberOfCourse;
+}
